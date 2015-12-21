318ae269873120661a51df8b86fc3d8ea348e736
HonoreN <h.nintunze@urbilog.fr>
2015-12-21 15:04:26 +0100

    improvment: added functions for android and browser

--- src/android/TTS.java
+++ src/android/TTS.java
@@ -39,42 +39,79 @@ public class TTS extends CordovaPlugin implements OnInitListener {
 
     @Override
     public void initialize(CordovaInterface cordova, final CordovaWebView webView) {
-        tts = new TextToSpeech(cordova.getActivity().getApplicationContext(), this);
-        tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
-            @Override
-            public void onStart(String s) {
-                // do nothing
-            }
+      ttsInitialized = false;
+      tts = new TextToSpeech(cordova.getActivity().getApplicationContext(), this);
+      tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+        @Override
+        public void onStart(String s) {
+          // do nothing
+        }
 
-            @Override
-            public void onDone(String callbackId) {
-                if (!callbackId.equals("")) {
-                    CallbackContext context = new CallbackContext(callbackId, webView);
-                    context.success();
-                }
-            }
+        @Override
+        public void onDone(String callbackId) {
+          if (!callbackId.equals("")) {
+              CallbackContext context = new CallbackContext(callbackId, webView);
+              context.success();
+          }
+        }
 
-            @Override
-            public void onError(String callbackId) {
-                if (!callbackId.equals("")) {
-                    CallbackContext context = new CallbackContext(callbackId, webView);
-                    context.error(ERR_UNKNOWN);
-                }
-            }
-        });
+        @Override
+        public void onError(String callbackId) {
+          if (!callbackId.equals("")) {
+              CallbackContext context = new CallbackContext(callbackId, webView);
+              context.error(ERR_UNKNOWN);
+          }
+        }
+      });
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
             throws JSONException {
         if (action.equals("speak")) {
-            speak(args, callbackContext);
-        } else {
-            return false;
+          speak(args, callbackContext);
+        }
+        else if (action.equals("interrupt")) {
+          interrupt(args, callbackContext);
+        }
+        else if (action.equals("stop")) {
+          stop(args, callbackContext);
+        }
+        else if (action.equals("silence")) {
+          silence(args, callbackContext);
+        }
+        else if (action.equals("speed")) {
+          speed(args, callbackContext);
+        }
+        else if (action.equals("pitch")) {
+          pitch(args, callbackContext);
+        }
+        else if (action.equals("startup")) {
+          startup(args, callbackContext);
+        }
+        else if (action.equals("shutdown")) {
+          shutdown(args, callbackContext);
+        }
+        else if (action.equals("getLanguage")) {
+          getLanguage(args, callbackContext);
+        }
+        else if (action.equals("isLanguageAvailable")) {
+          isLanguageAvailable(args, callbackContext);
+        }
+        else if (action.equals("setLanguage")) {
+          setLanguage(args, callbackContext);
+        }
+        else {
+          return false;
         }
         return true;
     }
 
+    /**
+     * Called when the TTS service is initialized.
+     *
+     * @param status
+     */
     @Override
     public void onInit(int status) {
         if (status != TextToSpeech.SUCCESS) {
@@ -83,62 +120,194 @@ public class TTS extends CordovaPlugin implements OnInitListener {
             // warm up the tts engine with an empty string
             HashMap<String, String> ttsParams = new HashMap<String, String>();
             ttsParams.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "");
-            tts.setLanguage(new Locale("en", "US"));
+            tts.setLanguage(new Locale("fr", "FR"));
             tts.speak("", TextToSpeech.QUEUE_FLUSH, ttsParams);
 
             ttsInitialized = true;
         }
     }
+    /**
+     * Clean up the TTS resources
+     */
+    public void onDestroy() {
+      if (tts != null) {
+        tts.shutdown();
+      }
+    }
+    /**
+     * Is the TTS service ready to play yet?
+     *
+     * @return
+     */
+    private boolean isReady() {
+      return ttsInitialized;
+    }
+
+    private String beforeSpeak(JSONArray args, CallbackContext callbackContext) {
+      JSONObject params = args.getJSONObject(0);
+
+      if (params == null) {
+          callbackContext.error(ERR_INVALID_OPTIONS);
+          return;
+      }
+
+      String text;
+      String locale;
+      double rate;
+
+      if (params.isNull("text")) {
+          callbackContext.error(ERR_INVALID_OPTIONS);
+          return null;
+      } else {
+          text = params.getString("text");
+      }
+
+      if (params.isNull("locale")) {
+          locale = "fr-FR";
+      } else {
+          locale = params.getString("locale");
+      }
+
+      if (params.isNull("rate")) {
+          rate = 1.0;
+      } else {
+          rate = params.getDouble("rate");
+      }
+
+      if (tts == null) {
+          callbackContext.error(ERR_ERROR_INITIALIZING);
+          return null;
+      }
+
+      if (!isReady()) {
+          callbackContext.error(ERR_NOT_INITIALIZED);
+          return null;
+      }
+
+      String[] localeArgs = locale.split("-");
+      tts.setLanguage(new Locale(localeArgs[0], localeArgs[1]));
+      tts.setSpeechRate((float) rate);
+      return text;
+    }
 
     private void speak(JSONArray args, CallbackContext callbackContext)
             throws JSONException, NullPointerException {
-        JSONObject params = args.getJSONObject(0);
-
-        if (params == null) {
-            callbackContext.error(ERR_INVALID_OPTIONS);
-            return;
-        }
-
-        String text;
-        String locale;
-        double rate;
-
-        if (params.isNull("text")) {
-            callbackContext.error(ERR_INVALID_OPTIONS);
-            return;
-        } else {
-            text = params.getString("text");
-        }
-
-        if (params.isNull("locale")) {
-            locale = "en-US";
-        } else {
-            locale = params.getString("locale");
-        }
-
-        if (params.isNull("rate")) {
-            rate = 1.0;
-        } else {
-            rate = params.getDouble("rate");
-        }
-
-        if (tts == null) {
-            callbackContext.error(ERR_ERROR_INITIALIZING);
-            return;
-        }
-
-        if (!ttsInitialized) {
-            callbackContext.error(ERR_NOT_INITIALIZED);
-            return;
-        }
-
+        String text = beforeSpeak(args,callbackContext);
+        if (text == null) return;
         HashMap<String, String> ttsParams = new HashMap<String, String>();
         ttsParams.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, callbackContext.getCallbackId());
 
-        String[] localeArgs = locale.split("-");
-        tts.setLanguage(new Locale(localeArgs[0], localeArgs[1]));
-        tts.setSpeechRate((float) rate);
+        tts.speak(text, TextToSpeech.QUEUE_ADD, ttsParams);
+    }
+    private void interrupt(JSONArray args, CallbackContext callbackContext)
+            throws JSONException, NullPointerException {
+        String text = beforeSpeak(args,callbackContext);
+        if (text == null) return;
+        HashMap<String, String> ttsParams = new HashMap<String, String>();
+        ttsParams.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, callbackContext.getCallbackId());
 
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, ttsParams);
+    }
+    private void stop(JSONArray args, CallbackContext callbackContext) {
+      if (tts == null) {
+          callbackContext.error(ERR_ERROR_INITIALIZING);
+          return null;
+      }
+
+      if (!isReady()) {
+          callbackContext.error(ERR_NOT_INITIALIZED);
+          return null;
+      }
+      tts.stop();
+    }
+    private void silence(JSONArray args, CallbackContext callbackContext) {
+      if (tts == null) {
+          callbackContext.error(ERR_ERROR_INITIALIZING);
+          return null;
+      }
+
+      if (!isReady()) {
+          callbackContext.error(ERR_NOT_INITIALIZED);
+          return null;
+      }
+      HashMap<String, String> ttsParams = new HashMap<String, String>();
+      ttsParams.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, callbackContext.getCallbackId());
+
+      tts.playSilence(args.getLong(0), TextToSpeech.QUEUE_ADD, ttsParams);
+    }
+    private void speed(JSONArray args, CallbackContext callbackContext) {
+      if (tts == null) {
+          callbackContext.error(ERR_ERROR_INITIALIZING);
+          return null;
+      }
+
+      if (!isReady()) {
+          callbackContext.error(ERR_NOT_INITIALIZED);
+          return null;
+      }
+      float speed = (float) (args.optLong(0, 100)) /(float) 100.0;
+      tts.setSpeechRate(speed);
+    }
+    private void pitch(JSONArray args, CallbackContext callbackContext) {
+      if (tts == null) {
+          callbackContext.error(ERR_ERROR_INITIALIZING);
+          return null;
+      }
+
+      if (!isReady()) {
+          callbackContext.error(ERR_NOT_INITIALIZED);
+          return null;
+      }
+      float pitch = (float) (args.optLong(0, 100)) /(float) 100.0;
+      tts.setPitch(pitch);
+    }
+    private void startup(JSONArray args, CallbackContext callbackContext) {
+      this.initialize(cordova, webView);
+    }
+    private void shutdown(JSONArray args, CallbackContext callbackContext) {
+      if (tts != null) tts.shutdown();
+    }
+    private void getLanguage(JSONArray args, CallbackContext callbackContext) {
+      if (tts == null) {
+        callbackContext.error(ERR_ERROR_INITIALIZING);
+        return null;
+      }
+
+      if (!isReady()) {
+        callbackContext.error(ERR_NOT_INITIALIZED);
+        return null;
+      }
+      String result = tts.getLanguage().toString();
+      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK,result));
+    }
+    private void isLanguageAvailable(JSONArray args, CallbackContext callbackContext) {
+      if (tts == null) {
+        callbackContext.error(ERR_ERROR_INITIALIZING);
+        return null;
+      }
+
+      if (!isReady()) {
+        callbackContext.error(ERR_NOT_INITIALIZED);
+        return null;
+      }
+      Locale loc = new Locale(args.getString(0));
+      int available = tts.isLanguageAvailable(loc);
+      String result = available < 0 ? "false" : "true";
+      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK,result));
+    }
+    private void setLanguage(JSONArray args, CallbackContext callbackContext) {
+      if (tts == null) {
+        callbackContext.error(ERR_ERROR_INITIALIZING);
+        return null;
+      }
+
+      if (!isReady()) {
+        callbackContext.error(ERR_NOT_INITIALIZED);
+        return null;
+      }
+      Locale loc = new Locale(args.getString(0));
+      int available = tts.setLanguage(loc);
+      String result = available < 0 ? "false" : "true";
+      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK,result));
     }
 }
--- www/tts.js
+++ www/tts.js
@@ -1,28 +1,136 @@
 /*
 
-    Cordova Text-to-Speech Plugin
-    https://github.com/vilic/cordova-plugin-tts
+  Cordova Text-to-Speech Plugin
+  https://github.com/vilic/cordova-plugin-tts
 
-    by VILIC VANE
-    https://github.com/vilic
+  by VILIC VANE
+  https://github.com/vilic
 
-    MIT License
+  MIT License
 
 */
 
-exports.speak = function (text, onfulfilled, onrejected) {
-    var options = {};
+/**
+ * Stop any queued synthesized speech
+ *
+ * @param {DOMString} text
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.speak = (text, successCallback, errorCallback) => {
+  var options = {};
 
-    if (typeof text == 'string') {
-        options.text = text;
-    } else {
-        options = text;
-    }
+  if (typeof text == 'string') {
+    options.text = text;
+  } else {
+    options = text;
+  }
 
-    cordova
-        .exec(function () {
-            onfulfilled();
-        }, function (reason) {
-            onrejected(reason);
-        }, 'TTS', 'speak', [options]);
+  cordova.exec(successCallback, errorCallback, 'TTS', 'speak', [options]);
+};
+/**
+ * Stop any queued synthesized speech
+ *
+ * @param {DOMString} text
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.interrupt = (text, successCallback, errorCallback) => {
+  var options = {};
+
+  if (typeof text == 'string') {
+    options.text = text;
+  } else {
+    options = text;
+  }
+
+  cordova.exec(successCallback, errorCallback, 'TTS', 'interrupt', [options]);
+};
+
+/**
+ * Stop any queued synthesized speech
+ *
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.stop = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "TTS", "stop", []);
+};
+/**
+ * Play silence for the number of ms passed in as duration
+ *
+ * @param {long} duration
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.silence = function(duration, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "TTS", "silence", [duration]);
+};
+/**
+ * Set speed of speech.  Usable from 30 to 500.  Higher values make little difference.
+ *
+ * @param {long} speed
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.speed = function(speed, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "TTS", "speed", [speed]);
+};
+/**
+ * Set pitch of speech.  Useful values are approximately 30 - 300
+ *
+ * @param {long} pitch
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.pitch = function(pitch, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "TTS", "pitch", [pitch]);
+};
+/**
+ * Starts up the TTS Service
+ *
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.startup = function(successCallback, errorCallback) {
+  console.log("TTS-Startup");
+    cordova.exec(successCallback, errorCallback, "TTS", "startup", []);
+};
+/**
+ * Shuts down the TTS Service if you no longer need it.
+ *
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.shutdown = function(successCallback, errorCallback) {
+     cordova.exec(successCallback, errorCallback, "TTS", "shutdown", []);
+};
+/**
+ * Finds out if the language is currently supported by the TTS service.
+ *
+ * @param {DOMString} lang
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.isLanguageAvailable = function(lang, successCallback, errorCallback) {
+     cordova.exec(successCallback, errorCallback, "TTS", "isLanguageAvailable", [lang]);
+};
+/**
+ * Finds out the current language of the TTS service.
+ *
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.getLanguage = function(successCallback, errorCallback) {
+     cordova.exec(successCallback, errorCallback, "TTS", "getLanguage", []);
+};
+/**
+ * Sets the language of the TTS service.
+ *
+ * @param {DOMString} lang
+ * @param {Object} successCallback
+ * @param {Object} errorCallback
+ */
+exports.setLanguage = function(lang, successCallback, errorCallback) {
+     cordova.exec(successCallback, errorCallback, "TTS", "setLanguage", [lang]);
 };
