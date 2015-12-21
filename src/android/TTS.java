3089370fbb683c3322e9601648c7ec449710fef3
HonoreN <h.nintunze@urbilog.fr>
2015-12-21 16:43:59 +0100

    startup should create new TTS object

--- src/android/TTS.java
+++ src/android/TTS.java
@@ -152,7 +152,7 @@ public class TTS extends CordovaPlugin implements OnInitListener {
             }
           });
         }
-        else {
+        /*else {
             // warm up the tts engine with an empty string
             HashMap<String, String> ttsParams = new HashMap<String, String>();
             ttsParams.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "");
@@ -160,7 +160,7 @@ public class TTS extends CordovaPlugin implements OnInitListener {
             tts.speak("", TextToSpeech.QUEUE_FLUSH, ttsParams);
 
             ttsInitialized = true;
-        }
+        }*/
     }
     /**
      * Clean up the TTS resources
